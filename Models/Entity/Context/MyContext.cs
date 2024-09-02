056780d760ec61421c7045a287c72bd627e581ca
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-09-02 13:09:27 -0300

    Incluido contexto e configurações do projeto

---
+++ Models/Entity/Context/MyContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace ControleVeiculos.Models.Entity.Context
+{
+    internal class MyContext : DbContext
+    {
+        private string _connectionString;
+        private readonly string _databaseProvider;
+
+        public MyContext()
+        {
+            _databaseProvider = ConfigurationManager.AppSettings["DatabaseProvider"];
+            _connectionString = ConfigurationManager.ConnectionStrings[_databaseProvider + "Connection"].ConnectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (_databaseProvider == "SQLite")
+            {
+                optionsBuilder.UseSqlite(_connectionString).UseLazyLoadingProxies();
+            }
+            else if (_databaseProvider == "PostgreSQL")
+            {
+                optionsBuilder.UseNpgsql(_connectionString).UseLazyLoadingProxies();
+            }
+            else
+            {
+                throw new InvalidOperationException("Database provider not supported.");
+            }
+        }
+    }
+}
---
+++ Models/Settings/ConfigurationManagerHelper.cs
@@ -0,0 +1,66 @@
+﻿using System.Configuration;
+
+
+namespace ControleVeiculos.Models.Settings
+{
+    internal class ConfigurationManagerHelper
+    {
+
+        public static string GetSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        public static void SetSetting(string key, string value)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            if (config.AppSettings.Settings[key] == null)
+            {
+                config.AppSettings.Settings.Add(key, value);
+            }
+            else
+            {
+                config.AppSettings.Settings[key].Value = value;
+            }
+
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        public static string GetConnectionString(string provider)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings[provider + "Connection"];
+            return connectionString?.ConnectionString;
+        }
+
+        public static void SetConnectionString(string provider, string connectionString)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            if (config.ConnectionStrings.ConnectionStrings[provider + "Connection"] == null)
+            {
+                config.ConnectionStrings.ConnectionStrings.Add(new ConnectionStringSettings
+                {
+                    Name = provider + "Connection",
+                    ConnectionString = connectionString,
+                    ProviderName = provider == "SQLite" ? "Microsoft.Data.Sqlite" : "Npgsql"
+                });
+            }
+            else
+            {
+                config.ConnectionStrings.ConnectionStrings[provider + "Connection"].ConnectionString = connectionString;
+            }
+
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("connectionStrings");
+        }
+
+        public static void SetDatabaseProvider(string provider)
+        {
+            SetSetting("DatabaseProvider", provider);
+        }
+    }
+}
